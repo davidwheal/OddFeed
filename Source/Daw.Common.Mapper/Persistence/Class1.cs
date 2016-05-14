5e0fc72bde5575f145e745ee9e5dd6d4f870f585
David Wheal <David Wheal>
2016-05-14 12:56:14 +0100

    wip

---
+++ Source/Daw.Common.Mapper/Persistence/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daw.Common.Mapper
+{
+    /// <summary>
+    /// https://www.mongodb.com/dr/fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-3.2.6-signed.msi/download
+    /// </summary>
+    public class Class1
+    {
+    }
+}
---
+++ StartMongo.cmd
@@ -0,0 +1,5 @@
+d:
+cd D:\Development\OddFeed
+D:\"Program Files"\MongoDB\Server\3.2\bin\Mongod --config "D:\Development\OddFeed\mongod.conf" --install
+net start "MongoDb"
+pause
